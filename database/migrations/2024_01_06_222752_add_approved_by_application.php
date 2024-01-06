1d0f03506c7d7530dfd1a5d70dd8f0ace7b7b3eb
mape.ally <mape.ally@gmail.com>
2024-01-07 01:05:58 +0300

    signature

---
+++ database/migrations/2024_01_06_222752_add_approved_by_application.php
@@ -0,0 +1,33 @@
+<?php
+
+use Doctrine\DBAL\Schema\Column;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApprovedByApplication extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->bigInteger('approved_by')->nullable()->after('approved_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+             $table->dropColumn('approved_by');
+        });
+    }
+}
