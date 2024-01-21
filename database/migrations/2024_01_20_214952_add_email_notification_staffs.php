662693d2da7d74a90880d64b57850583b34adcc3
mape.ally <mape.ally@gmail.com>
2024-01-21 16:41:02 +0300

    add email notification

---
+++ database/migrations/2024_01_20_214952_add_email_notification_staffs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Doctrine\DBAL\Schema\Column;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmailNotificationStaffs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('staffs', function (Blueprint $table) {
+            $table->boolean('email_notify')->default(false)->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('staffs', function (Blueprint $table) {
+            $table->dropColumn('email_notify');
+        });
+    }
+}
